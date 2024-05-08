7052c5323437f6de2ac38e8de8259a825fb4dcd8
Alexzvn <personal@alexzvn.me>
2024-05-08 18:19:04 +0700

    fix proxy method

--- src/bundler/client/worker.esm.ts
+++ src/bundler/client/worker.esm.ts
@@ -15,25 +15,25 @@ declare const __HMR_PORT__: string
 
 
 const extensionOrigin = new URL(chrome.runtime.getURL('/')).origin
+const proxy = async (url: URL) => {
+  url.protocol = 'http:'
+  url.host = 'localhost'
+  url.port = __HMR_PORT__
+
+  url.searchParams.set('t', Date.now().toString())
+  const res = await fetch(url.href.replace(/=$|=(?=&)/g, ''))
+
+  return new Response(res.body, {
+    headers: {
+    'Content-Type': res.headers.get('Content-Type') ?? 'text/javascript'
+    }
+  })
+}
+
 self.addEventListener('fetch', (event: any) => {
   const url= new URL(event.request.url)
 
   if (url.origin === extensionOrigin) {
-    console.log(url.href)
-
-    event.respondWith(async (url: URL) => {
-      url.protocol = 'http:'
-      url.host = 'localhost'
-      url.port = __HMR_PORT__
-    
-      url.searchParams.set('t', Date.now().toString())
-      const res = await fetch(url.href.replace(/=$|=(?=&)/g, ''))
-    
-      return new Response(res.body, {
-        headers: {
-          'Content-Type': res.headers.get('Content-Type') ?? 'text/javascript'
-        }
-      })
-    })
+    event.respondWith(proxy(url))
   }
 })
