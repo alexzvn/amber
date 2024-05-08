d71564e2a08e8d5870f78e958b2247cff12d2f21
Alexzvn <personal@alexzvn.me>
2024-05-08 14:59:55 +0700

    clean up

--- src/bundler/configure.ts
+++ src/bundler/configure.ts
@@ -1,10 +1,5 @@
-import {type UserConfig, defineConfig as defineVite, mergeConfig} from 'vite'
-import {join} from 'path'
+import {type UserConfig} from 'vite'
 import type {GeneralManifest} from '~/bundler/browsers/manifest'
-import ContentScript from '~/bundler/components/ContentScript'
-import Page from '~/bundler/components/Page'
-import BackgroundScript from '~/bundler/components/BackgroundScript'
-import program from './cli/program'
 
 export type ExtensionOptions = {
   devManifest?: Partial<GeneralManifest>
