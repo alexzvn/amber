c862422025cccb1fd3395fb839b177e9c7c042e1
Alexzvn <personal@alexzvn.me>
2024-06-22 17:55:12 +0700

    add clean .amber folder

--- src/bundler/cli/clean.ts
+++ src/bundler/cli/clean.ts
@@ -1,19 +1,30 @@
 import { program, cwd } from './program'
 import fs from 'fs/promises'
-import {join} from "path";
+import {join} from 'path'
+
+const check = async () => {
+  try {
+    await fs.access(join(cwd, 'amber.config.ts'))
+      .catch(() => fs.access(join(cwd, 'amber.config.js')))
+  } catch (e) {
+    console.error('Amber config not found')
+    process.exit(1)
+  }
+}
+
+const clean = {
+  dist: () => fs.rm(join(cwd, 'dist'), { recursive: true, force: true }),
+  amber: () => fs.rm(join(cwd, '.amber'), { recursive: true, force: true }),
+}
 
 program.command('clean')
   .description('Clean output folder')
   .action(async () => {
-    try {
-      await fs.access(join(cwd, 'amber.config.ts'))
-        .catch(() => fs.access(join(cwd, 'amber.config.js')))
-    } catch (e) {
-      console.error('Amber config not found')
-      process.exit(1)
-    }
+    await check().then(clean.dist)
+  })
 
-    await fs.rm(join(cwd, 'dist'), {
-      recursive: true, force: true
-    })
+program.command('cleanup')
+  .description('Clean up everything')
+  .action(async () => {
+    await check().then(clean.dist).then(clean.amber)
   })
