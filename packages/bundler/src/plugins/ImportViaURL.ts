a4d364a96b34e2ca95c5398349878f3ca0c9bc5f
Alexzvn <personal@alexzvn.me>
2024-07-13 23:59:13 +0700

    allow compile types for url

--- packages/bundler/src/plugins/ImportViaURL.ts
+++ packages/bundler/src/plugins/ImportViaURL.ts
@@ -2,6 +2,7 @@ import { code } from '../../../amber/src/hashing/Hash'
 import { defineVitePlugin, exists, mkdir } from '../helper'
 import fs from 'fs/promises'
 import { join } from 'path'
+import ts from 'typescript'
 
 const parseURL = (path: string) => {
   try {
@@ -17,9 +18,48 @@ const parseURL = (path: string) => {
   return undefined
 }
 
+const tab = (source: string, size = 2) => {
+  const space = ' '.repeat(size)
+
+  return source.split('\n').map(line => space + line).join('\n')
+}
+
+/**
+ * compile dts
+ */
+const compile = (filepath: string, callback: (dts: string) => unknown) => {
+  const extensions = ['.js', '.ts', '.mjs', '.mts', '.cts']
+
+  const isCompatible = extensions.some(ext => filepath.endsWith(ext))
+  const empty = [
+    'const content: any',
+    'export default content'
+  ].join('\n')
+
+  if (!isCompatible) {
+    return callback(tab(empty, 4))
+  }
+
+  const compiler = ts.createProgram({
+    rootNames: [filepath],
+    options: {
+      allowJs: true,
+      emitDeclarationOnly: true,
+      declaration: true,
+      checkJs: false,
+      resolveJsonModule: true,
+      esModuleInterop: true,
+    }
+  })
+
+  compiler.emit(undefined, (_, text) => callback(tab(text, 4)))
+}
+
 export default defineVitePlugin(async () => {
   const base = join('.amber', 'cache')
+  const typing = join('.amber', 'types')
   await mkdir(base)
+  await mkdir(typing)
 
   return {
     name: 'amber:import-url',
@@ -36,16 +76,27 @@ export default defineVitePlugin(async () => {
         const response = await fetch(url.toString())
         const filename = url.pathname.split('/').pop()
         const target = `${code(url.toString())}-${filename}`
+        const filepath = join(base, target)
 
-        if (await exists(join(base, target))) {
+        if (await exists(filepath)) {
           this.debug('Cache hit ' + url)
           return `.amber/cache/${target}`
         }
-    
+
         this.info('Downloading ' + url)
-        await fs.writeFile(join(base, target), await response.text())
+        await fs.writeFile(filepath, await response.text())
         this.info('Downloaded ' + url)
-    
+
+        compile(filepath, (text) => {
+          const declaration = [
+            `declare module ${JSON.stringify(url)} {`,
+            text,
+            `}`
+          ]
+  
+          fs.writeFile(join(typing, `${target}.d.ts`), declaration.join('\n'))
+        })
+
         return `.amber/cache/${target}`
       }
 
