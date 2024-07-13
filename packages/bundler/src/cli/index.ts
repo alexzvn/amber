293adfbc7792f1457cadb1a049fdae55482432af
Alexzvn <personal@alexzvn.me>
2024-07-13 20:04:19 +0700

    add back shebang cause it's not supported without it

--- packages/bundler/src/cli/index.ts
+++ packages/bundler/src/cli/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import 'dotenv/config'
 
 import program from './program'
@@ -5,5 +6,22 @@ import './dev'
 import './build'
 import './archive'
 import './clean'
+
+console.log(process.env);
+
+
+// Detect if running in Bun or Node.js
+const isBun = typeof Bun !== 'undefined';
+const isNode = typeof process !== 'undefined' && process.versions && process.versions.node;
+
+if (isBun) {
+  console.log('Running in Bun');
+  // Bun-specific code here
+} else if (isNode) {
+  console.log('Running in Node.js');
+  // Node.js-specific code here
+} else {
+  console.error('Unsupported environment');
+}
 
 program.parse()
