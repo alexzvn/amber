32c017020dbf00b298e3858673e742eb2e06b80e
Alexzvn <personal@alexzvn.me>
2024-07-13 20:05:52 +0700

    oops forget remove debug log

--- packages/bundler/src/cli/index.ts
+++ packages/bundler/src/cli/index.ts
@@ -7,21 +7,4 @@ import './build'
 import './archive'
 import './clean'
 
-console.log(process.env);
-
-
-// Detect if running in Bun or Node.js
-const isBun = typeof Bun !== 'undefined';
-const isNode = typeof process !== 'undefined' && process.versions && process.versions.node;
-
-if (isBun) {
-  console.log('Running in Bun');
-  // Bun-specific code here
-} else if (isNode) {
-  console.log('Running in Node.js');
-  // Node.js-specific code here
-} else {
-  console.error('Unsupported environment');
-}
-
 program.parse()
