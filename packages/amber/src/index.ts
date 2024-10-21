ad16196ea9e9a59cd9170fdd541cf968edd7bfa6
Alexzvn <personal@alexzvn.me>
2024-10-21 21:30:33 +0700

    feat: simple selector helper

--- packages/amber/src/index.ts
+++ packages/amber/src/index.ts
@@ -1,9 +1,10 @@
 import Messaging, { defineMessagingAddon } from './messaging/Messaging'
-export type { AsyncReadableStreamEvent } from './messaging/Channel.ts'
 import Storage from './storage/Storage'
 import * as Misc from './misc/Misc'
 import * as Hash from './hashing/Hash.ts'
 import { defineSimpleQueue } from './queue/SimpleQueue.ts'
+export type { AsyncReadableStreamEvent } from './messaging/Channel.ts'
+export { $, $$, SequenceError } from './selector/Selector.ts'
 
 export {
   Storage,
