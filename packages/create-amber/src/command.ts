da8826243b0c5cf8d97b9e24deaaa8260436f322
Alexzvn <personal@alexzvn.me>
2024-11-27 13:40:35 +0700

    allow to specify folder name

--- packages/create-amber/src/command.ts
+++ packages/create-amber/src/command.ts
@@ -1,21 +1,31 @@
 import { Command } from 'commander'
 import prompts from 'prompts'
-import { create, type CreateAmber } from './create'
+import { create } from './create'
 import { version, name } from '../package.json'
 
 const program = new Command()
   .name(name)
   .description('Create amber project')
   .version(version)
-  .action(async () => {
-    console.clear()
+  .argument('[folder]', 'Your project name folder')
+  .action(async (folder?: string) => {
 
-    const answer = await prompts([
-      { type: 'text', name: 'folder', message: 'Project name:', initial: 'amber-project' },
-      { type: 'confirm', name: 'devBrowser', message: 'Use isolated chromium browser for development?', initial: true }
-    ])
+    folder ??= await prompts({
+      type: 'text',
+      name: 'folder',
+      message: 'Project name:',
+      initial: 'amber-project'
+    }).then(it => it.folder)
 
-    answer.folder && await create(answer satisfies CreateAmber)
+    const { devBrowser } = await prompts({
+      type: 'confirm', name: 'devBrowser',
+      message: 'Use isolated chromium browser for development?',
+      initial: true
+    })
+
+    if (folder) {
+      await create({ folder, devBrowser })
+    }
   })
 
 program.parse()
